5b234ec654a48192d06b2ec41c117b469a2df026
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2023-01-23 20:10:32 +0700

    host

--- env/index.js
+++ env/index.js
@@ -3,7 +3,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.PORT = void 0;
+exports.PORT = exports.CYCLIC_URL = void 0;
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
+exports.CYCLIC_URL = process.env.CYCLIC_URL;
 exports.PORT = process.env.PORT;
