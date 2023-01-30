e21811a3eea1f347e4b4d01297caa7c63ca036a3
Farhan Pradana Tallei <farhan.pradana55@gmail.com>
2023-01-30 17:35:08 +0700

    cors

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.addRoutes = exports.addPlugins = void 0;
+const cors_1 = __importDefault(require("@fastify/cors"));
 const sensible_1 = __importDefault(require("@fastify/sensible"));
 const fastify_1 = __importDefault(require("fastify"));
 const routes_1 = require("./routes");
 const app = (0, fastify_1.default)().withTypeProvider();
 function addPlugins() {
     app.register(sensible_1.default);
+    app.register(cors_1.default, { origin: '*' });
 }
 exports.addPlugins = addPlugins;
 function addRoutes() {
